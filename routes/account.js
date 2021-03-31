d498f72efc2829bc41eb1d0f627eed08a5989dba
eyjafallaa <ysson51@naver.com>
2021-03-31 19:37:27 +0900

    restore

--- routes/account.js
+++ routes/account.js
@@ -9,8 +9,9 @@ router.get('/sign_up',(req,res,next)=>{
   db.query('INSERT INTO (userid,name,password,nickname,email,schoolname,contact,enteryear,profilepicture) VALUES(?,?,?,?,?,?,?,?,?)',
   [post.userid,post.name,post.password,post.nickname,post.email,post.schoolname,post.contact,post.enteryear,post.profilepicture],(err,result)=>{
     //multer 사용해서 잘 처리
+
     res.status(200).json({
-      //로그인 토큰 준다.
+      "token":,
     });
   })
 })
--- routes/index.js
+++ routes/index.js
@@ -7,10 +7,35 @@ router.get('/', function(req, res, next) {
 });
 
 router.get('/search', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.status(200).json([
+    {
+      post:"1234",
+      title:"이물건팝니다",
+      tag:"태그1 태그2 태그3 태그5",
+      userid:"kseocken",
+      price:"30230300",
+      timestamp:"2020-04-04-11-23-60",
+      attachment:["url1","url2","url3","url4"]
+    }
+  ])
 });
 
-router.get('/schoolinfo', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-}); 
+router.get('/schoolinfo', function(req, res, next) {//학교검색하기
+  res.status(200).json([
+    {
+      school_code:"대충학교코드",
+      school_name="대고소푸토웨아고등학교",
+      school_location="대구 구지 구지로 182",
+    },
+    {
+      school_code:"대충학교코드2",
+      school_name="대고소푸토웨아고등학교2",
+      school_location="대구 구지 구지로 182+2",
+    },
+    {
+      school_code:"대충학교코드3",
+      school_name="대고소푸토웨아고등학교3",
+      school_location="대구 구지 구지로 182+3",
+    }
+  ])
 module.exports = router;
