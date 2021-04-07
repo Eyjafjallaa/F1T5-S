7fc3f5731120fa6169370a8c3f0c65a6161c874d
eyjafallaa <ysson51@naver.com>
2021-04-07 20:05:53 +0900

    Revert "Update profile.js"
    This reverts commit 31ec37ca52bcb7976cdfc31ae9285b06c1e35d99.

--- routes/profile.js
+++ routes/profile.js
@@ -1,16 +1,9 @@
 var express = require('express');
 var router = express.Router();
-<<<<<<< HEAD
 
 /* GET users listing. */
 
 router.get('/:userid', function(req, res, next) {//조회
-=======
-const upload = require('../middleware/fileload');
-/* GET users listing. */
-
-router.get('/:postid', function(req, res, next) {//조회
->>>>>>> parent of dfdeec4 (Merge branch 'master' of https://github.com/Eyjafjallaa/F1T5-S)
     res.status(200).json({
         school_name:"대구소프트웨어마이스터고등학고",
         name:"강석형",
@@ -21,11 +14,7 @@ router.get('/:postid', function(req, res, next) {//조회
     });
 });
 
-<<<<<<< HEAD
 router.put('/:userid', function(req, res, next) {//수정
-=======
-router.put('/:postid',upload.single() ,function(req, res, next) {//수정
->>>>>>> parent of dfdeec4 (Merge branch 'master' of https://github.com/Eyjafjallaa/F1T5-S)
     res.status(200).json({
         school_name:"대구소프트웨어마이스터고등학고",
         name:"강석형",
