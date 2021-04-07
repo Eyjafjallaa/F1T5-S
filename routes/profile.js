0626828d1ca50de2ba1dd8b522c35432b62f1653
tmdeh <yus174113@gmail.com>
2021-04-07 19:21:28 +0900

    profile

--- routes/profile.js
+++ routes/profile.js
@@ -1,17 +1,10 @@
 var express = require('express');
 var router = express.Router();
-
-/* GET users listing. */
+const db = require("../model/db");
+const jwt = require('jsonwebtoken');
 
 router.get('/:userid', function(req, res, next) {//조회
-    res.status(200).json({
-        school_name:"대구소프트웨어마이스터고등학고",
-        name:"강석형",
-        nickname:"귀염뽀짝석현이",
-        contact:"000-0000-0000",
-        enteryear:"2019",
-        profilepicture:"대충URL같은거"
-    });
+    
 });
 
 router.put('/:userid', function(req, res, next) {//수정
