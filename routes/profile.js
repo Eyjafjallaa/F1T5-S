084b4065cfbf0db82a4c3d60c9d3803412175934
eyjafallaa <ysson51@naver.com>
2021-04-07 19:59:38 +0900

    Create profile.js

---
+++ routes/profile.js
@@ -0,0 +1,28 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET users listing. */
+
+router.get('/:userid', function(req, res, next) {//조회
+    res.status(200).json({
+        school_name:"대구소프트웨어마이스터고등학고",
+        name:"강석형",
+        nickname:"귀염뽀짝석현이",
+        contact:"000-0000-0000",
+        enteryear:"2019",
+        profilepicture:"대충URL같은거"
+    });
+});
+
+router.put('/:userid', function(req, res, next) {//수정
+    res.status(200).json({
+        school_name:"대구소프트웨어마이스터고등학고",
+        name:"강석형",
+        nickname:"귀염뽀짝석현이",
+        contact:"000-0000-0000",
+        enteryear:"2019",
+        profilepicture:"대충URL같은거"
+    });
+});
+
+module.exports = router;
