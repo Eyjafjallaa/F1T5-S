3e05ed33fe676045f74ab50e6c2f893c13b2397f
tmdeh <yus174113@gmail.com>
2021-04-07 18:17:09 +0900

    login

--- routes/account.js
+++ routes/account.js
@@ -29,7 +29,31 @@ router.post('/sign_up', (req, res, next) => {
 })
 
 router.post('/login', (req, res) => {
-
+  let post = req.body;
+  const pw = crypto.createHash('sha512').update(post.password).digest('base64'); //암호화된 리퀘스트 패스워드
+  db.query(`select password from user where userid=?`,[post.userid],(err,result)=>{
+    if(result[0]==undefined){
+      res.status(401).json();
+      return;
+    }
+    if(pw==result[0].password){
+      var user = { 
+        sub: post.userid,
+        name:post.nickname,
+        iat:new Date().getTime()/1000
+      };
+      var token = jwt.sign(user, secret, {
+        expiresIn: "1m"
+      })
+      res.status(200).json({
+        logintoken:token,
+      });
+    }
+    else{
+      res.status(401).json();
+    }
+  })
+  
 })
 
 router.post('/sign_up/check', (req, res) => {
