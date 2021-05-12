29c8318155e68b66e5b5a4c55e09eaff74a360ea
eyjafallaa <ysson51@naver.com>
2021-05-12 18:12:27 +0900

    update seller{userid}

--- routes/seller.js
+++ routes/seller.js
@@ -1,62 +1,42 @@
 var express = require('express');
 var router = express.Router();
+const db = require('../model/db');
 
-router.get('/seller/:userid', function(req, res, next) {
+router.get('/:userid', function (req, res, next) {
+  db.query(`SELECT * FROM user WHERE userid = ?`, [req.params.userid], (err, result) => {
+    if (err) {
+      res.status(400).json({ error: err });
+      return;
+    }
     res.status(200).json({
-      nickname:"nicname",
-      schoolname:"dgswhs",
-      prifilepicture:"대충사진URL"
-    })
+      nickname: result[0].nickname,
+      schoolname: result[0].schoolname,
+      prifilepicture: result[0].profilepicture,
+    });
+  })
 });
 
-router.get('/seller/:userid/selling', function(req, res, next) {
+router.get('/:userid', function (req, res, next) {
+  
   res.status(200).json([
-  {
-    id : "1",
-    title : "post 제목",
-    tag : "#태그",
-    nickname : "nickname",
-    price : "3000",
-    timestamp : "2021-03-31 17:30:36"
-  },
-  {
-    id : "2",
-    title : "post 제목2",
-    tag : "#태그2",
-    nickname : "nickname2",
-    price : "4000",
-    timestamp : "2021-03-31 17:30:37"
-  }
-])
-});
-
-router.get('/seller/:userid/selled', function(req, res, next) {
-    res.status(200).json([
     {
-        id : "1",
-        title : "제목",
-        tag : "#태그 #태그1",
-        userid : "userid",
-        nickname : "nickname",
-        price : "3000",
-        timestamp : "2021-03-31 17:30:38",
-        attachment : [
-            "대충 URL1","대충 URL2","대충 URL3",
-        ]
+      id: "1",
+      title: "post 제목",
+      tag: "#태그",
+      nickname: "nickname",
+      price: "3000",
+      timestamp: "2021-03-31 17:30:36"
     },
     {
-        id : "2",
-        title : "제목2",
-        tag : "#태그2 #태그22",
-        userid : "userid2",
-        nickname : "nickname2",
-        price : "4000",
-        timestamp : "2021-03-31 17:30:39",
-        attachment : [
-            "대충 URL11","대충 URL22","대충 URL33",
-        ]
+      id: "2",
+      title: "post 제목2",
+      tag: "#태그2",
+      nickname: "nickname2",
+      price: "4000",
+      timestamp: "2021-03-31 17:30:37"
     }
-]);
-  });
+  ])
+});
+
 
 module.exports = router;
