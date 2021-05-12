0d426cbbf006b57b954a10a6bed69cf043728bbd
tmdeh <yus174113@gmail.com>
2021-05-12 20:51:41 +0900

    schoolinfo1

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,8 @@
 var express = require('express');
 var router = express.Router();
+let request = require('request');
+let cheerio = require('cheerio');
+const { data } = require('cheerio/lib/api/attributes');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -21,7 +24,24 @@ router.get('/search', function(req, res, next) {
 });
 
 router.get('/schoolinfo', function(req, res, next) {//학교검색하기
-  res.status(200).json([
+  let school = req.query.schoolName;
+  let url = "https://open.neis.go.kr/hub/schoolInfo?KEY=f08d52196c3a48ecbd679177083b65f0&Type=json&SCHUL_NM=" + encodeURI(school);
+  request(url, (err, response, body) => {
+    if(err){
+      return res.status(400).json({err});
+    }
+    //let bodyJson = JSON.parse(body);
+    //console.log(bodyJson);
+    console.log(typeof(body));
+    let jsonbody = JSON.parse(body);
+    console.log(jsonbody);
+    
+      console.log(jsonbody.schoolInfo[1].row[0]);
+    
+  });
+
+  res.status(200).json({});
+  /*res.status(200).json([
     {
       school_code:"대충학교코드",
       school_name:"대고소푸토웨아고등학교",
@@ -37,7 +57,7 @@ router.get('/schoolinfo', function(req, res, next) {//학교검색하기
       school_name:"대고소푸토웨아고등학교3",
       school_location:"대구 구지 구지로 182+3",
     }
-  ])
+  ])*/
 });
 
 module.exports = router;
