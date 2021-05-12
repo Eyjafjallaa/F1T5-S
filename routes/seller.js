c49e2b81ee6ba7ff60b200e91bdc2ba3a7b4a589
eyjafallaa <ysson51@naver.com>
2021-05-12 20:31:45 +0900

    seller finisihed

--- routes/seller.js
+++ routes/seller.js
@@ -16,27 +16,75 @@ router.get('/:userid', function (req, res, next) {
   })
 });
 
-router.get('/:userid', function (req, res, next) {
-  
-  res.status(200).json([
-    {
-      id: "1",
-      title: "post 제목",
-      tag: "#태그",
-      nickname: "nickname",
-      price: "3000",
-      timestamp: "2021-03-31 17:30:36"
-    },
-    {
-      id: "2",
-      title: "post 제목2",
-      tag: "#태그2",
-      nickname: "nickname2",
-      price: "4000",
-      timestamp: "2021-03-31 17:30:37"
+router.get('/:userid/products', function (req, res, next) {
+  const search_post = ()=>{
+    var step;
+    if(req.query.step=="0"){
+      step=0;
+    }else{
+      step=1;
     }
-  ])
+    const promise = new Promise((resolve, reject)=>{
+      db.query(`SELECT post.postid,post.title,post.tag,post.userid,post.price,post.timestamp,user.nickname,
+      group_concat(attachment.url ORDER by attachment.attachmentid) AS URL 
+      FROM post LEFT JOIN user ON post.userid=user.userid
+      LEFT JOIN attachment on attachment.postid=post.postid
+      WHERE user.userid=? AND post.step=?
+      GROUP BY post.postid 
+      ORDER BY timestamp DESC
+      `,[req.params.userid,step],(err,result)=>{
+        if(err)reject(err);
+        else{
+          var arr_result=[];
+          for(var i=0;i<result.length;i++){
+            arr_result.push({
+              postid: result[i].postid,
+              title: result[i].title,
+              tag: result[i].tag,
+              userid: result[i].userid,
+              nickname: result[i].nickname,
+              price: result[i].price,
+              timestamp: result[i].timestamp,
+              attachment: result[i].URL,
+            })
+          }
+          resolve(arr_result);
+        }
+      })
+    })
+    return promise;
+  }
+
+  const substr_URL = (result) => {
+    const promise = new Promise((resolve, reject) => {
+      var arr_result = [];
+      for (var i = 0; i < result.length; i++) {
+        if(result[i].attachment==undefined)continue;
+        const a=result[i].attachment.split(',');
+        console.log(a);
+        result[i].attachment=a;
+      }
+      resolve(result);
+    })
+    return promise;
+  }
+
+  const respond = (result) => {
+    res.status(200).json(result);
+  }
+
+  const error = (error) => {
+    res.status(500).json({ error: error });
+  }
+
+  search_post()
+    .then(substr_URL)
+    .then(respond)
+    .catch(error);
 });
+
+
+
 
 
 module.exports = router;
